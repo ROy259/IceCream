c4847db4f7e5247a76a9ed7caf28b3311381c66b
Rohan Panchal <rohanpanchal03790@gmail.com>
2024-12-13 16:48:25 +0530

    instaled icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: { colors: { crimsonRed: "#990000" } },
   },
   plugins: [],
-}
-
+};
